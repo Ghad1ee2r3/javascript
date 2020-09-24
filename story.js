178a1128361aeec49c5211e0be3363e4e224bf52
ghadeer <apps02977@gmail.com>
2020-09-24 13:08:01 +0300

    Done

---
+++ story.js
@@ -0,0 +1,5 @@
+let NAME = 'ghadeer';
+let age =22;
+let HOBBY ='drawing';
+let AGE_PLUS_33 =age +33;
+console.log(` Mr./Mrs. ${NAME}, in 33 years, you will be ${AGE_PLUS_33} years old. By then you ll have decades of ${HOBBY} practice!`);
